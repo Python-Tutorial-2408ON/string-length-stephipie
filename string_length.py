e8a5e582ca0f6269817acbf94cada3474d3ab3b3
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2024-10-09 21:41:08 +0200

    Update string_length.py

--- string_length.py
+++ string_length.py
@@ -1,5 +1,6 @@
 # Define a variabel e.g. string = ("hello") or in this case empty
 string = ("")
 # usage of built-in-function len()
-print(len(string))
+def string_length(string):
+  print(len(string))
 #print the built-in-function len() of the parameter string, to show the length of the string
