18fd31d08fff7e319671746246599045af725136
stephipie <stephi.pietschmann@gmail.com>
2024-10-09 22:08:57 +0200

    modyfied code

--- string_length.py
+++ string_length.py
@@ -1,5 +1,12 @@
 # Define a variabel e.g. string = ("hello") or in this case empty
-string = ("")
-# usage of built-in-function len()
-print(len(string))
+string = ""
+
+def string_length(string):
+    return len(string)  #use built-in-function len
+# Define a variable for length
+length = string_length(string)
+
+# print the length of the string
+print(length)
+
 #print the built-in-function len() of the parameter string, to show the length of the string
